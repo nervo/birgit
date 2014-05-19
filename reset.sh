056c3f5a9ea16519ea19e406ef8898dc58197059
nervo <nervo@nervo.net>
2014-05-19 07:49:55 +0200

    Ah oui, j'avais oublié, c'est pratique, ca

--- reset.sh
+++ reset.sh
@@ -1,4 +1,5 @@
 app/console doctrine:database:drop --force && \
 app/console doctrine:database:create && \
 app/console doctrine:schema:create && \
-app/console doctrine:fixtures:load -n
+#app/console doctrine:fixtures:load -n
+app/console birgit:test:fixtures
