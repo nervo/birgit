cebb07dc316381bf389aeb7fb1c2dc3a39443569
Florian Rey <florian.rey@elao.com>
2014-06-09 15:39:04 +0200

    Use webpack

--- src/Birgit/Front/Bundle/Bundle/Controller/TaskController.php
+++ src/Birgit/Front/Bundle/Bundle/Controller/TaskController.php
@@ -5,6 +5,7 @@ namespace Birgit\Front\Bundle\Bundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class TaskController extends Controller
 {
@@ -16,13 +17,23 @@ class TaskController extends Controller
      */
     public function indexAction()
     {
+        return array();
+    }
+
+    /**
+     * Index Json
+     *
+     * @Route("/task.json")
+     */
+    public function indexJsonAction()
+    {
         $links = array();
-        
+
         $taskQueues = $this->get('birgit.task_queue_repository')
             ->all();
-        
+
         foreach ($taskQueues as $taskQueue) {
-            
+
             foreach ($taskQueue->getPredecessors() as $predecessor) {
                 $links[] = array(
                     'source' => substr($predecessor->getId(), 0, 8),
@@ -38,11 +49,9 @@ class TaskController extends Controller
                     'type'   => 'successor'
                 );
             }
-            
+
         }
-        
-        return array(
-            'links' => $links
-        );
+
+        return new JsonResponse($links);
     }
 }
