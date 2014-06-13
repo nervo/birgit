6aeaeb27dd5b97e7a5de83e4c45e3089a939f2d1
Florian Rey <florian.rey@elao.com>
2014-06-13 10:58:26 +0200

    Api

--- src/Birgit/Api/Bundle/Bundle/Controller/TaskController.php
+++ src/Birgit/Api/Bundle/Bundle/Controller/TaskController.php
@@ -15,7 +15,7 @@ class TaskController extends Controller
     /**
      * Project
      * 
-     * @Route("/project")
+     * @Route("/project", name="api_task_project")
      */
     public function projectAction(Request $request)
     {
@@ -31,6 +31,65 @@ class TaskController extends Controller
         $taskQueue = $taskManager
             ->createProjectTaskQueue($project, [
                 'project'
+            ]);
+
+        // Push task queue
+        $taskManager->pushTaskQueue($taskQueue);
+
+        return new Response();
+    }
+
+    /**
+     * Project Reference
+     *
+     * @Route("/project/reference", name="api_task_project_reference")
+     */
+    public function projectReferenceAction(Request $request)
+    {
+        // Get project
+        $project = $this->get('birgit.model_repository_manager')
+            ->getProjectRepository()
+            ->get($request->get('projectName'));
+
+        // Get project reference
+        $projectReference = $this->get('birgit.model_repository_manager')
+            ->getProjectReferenceRepository()
+            ->get($request->get('name'), $project);
+
+        // Get task manager
+        $taskManager = $this->get('birgit.task_manager');
+
+        // Create task queue
+        $taskQueue = $taskManager
+            ->createProjectReferenceTaskQueue($projectReference, [
+                'project_reference'
+            ]);
+
+        // Push task queue
+        $taskManager->pushTaskQueue($taskQueue);
+
+        return new Response();
+    }
+
+    /**
+     * Host
+     *
+     * @Route("/host", name="api_task_host")
+     */
+    public function hostAction(Request $request)
+    {
+        // Get host
+        $host = $this->get('birgit.model_repository_manager')
+            ->getHostRepository()
+            ->get($request->get('id'));
+
+        // Get task manager
+        $taskManager = $this->get('birgit.task_manager');
+
+        // Create task queue
+        $taskQueue = $taskManager
+            ->createHostTaskQueue($host, [
+                'host'
             ]);
 
         // Push task queue
--- src/Birgit/Front/Bundle/Bundle/Resources/assets/js/project.js
+++ src/Birgit/Front/Bundle/Bundle/Resources/assets/js/project.js
@@ -1,3 +1,8 @@
 require(['jquery/dist/jquery'], function($) {
-
+    $(function() {
+        $('a.api').click(function(event) {
+            $.ajax(event.target.href);
+            event.preventDefault();
+        });
+    });
 });
