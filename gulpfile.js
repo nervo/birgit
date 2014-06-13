9ad252ff3565502766408040b482c3db589b6d54
Florian Rey <florian.rey@elao.com>
2014-06-13 09:45:07 +0200

    Gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ var
         cache: true,
         context: __dirname + '/src',
         entry: {
+            project: './Birgit/Front/Bundle/Bundle/Resources/assets/js/project',
             task: './Birgit/Front/Bundle/Bundle/Resources/assets/js/task'
         },
         output: {
@@ -40,6 +41,10 @@ gulp.task('css', function() {
         'bower_components/bootstrap/dist/css/bootstrap.css'
     ])
         .pipe(gulp.dest('web/assets/css'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('src/**/*.js', ['webpack']);
 });
 
 gulp.task('default', ['css', 'webpack'], function() {
