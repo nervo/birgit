0257446bd80904468dae23013ae5195187dca94b
nervo <nervo@nervo.net>
2014-06-17 09:42:39 +0200

    Replage reset script & supervisor configuration by gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -62,5 +62,77 @@ gulp.task('watch', function() {
     gulp.watch('src/**/*.js', ['webpack']);
 });
 
+gulp.task('reset', function(callback) {
+    var
+        execs = [];
+
+    [
+        ['doctrine:database:drop --force', false],
+        ['doctrine:database:create', true],
+        ['doctrine:schema:create --em=birgit', true],
+        ['doctrine:schema:create --em=birgit_task', true],
+        ['doctrine:schema:create --em=birgit_event', true],
+        ['birgit:test:fixtures', true]
+    ].forEach(function(parameters) {
+        execs.push(function(command, failOnError, callback) {
+            gulpUtil.log('Exec', gulpUtil.colors.green(command));
+            require('child_process')
+                .exec('bin/console ' + command + ' --ansi', function(error, stdout, stderr) {
+                    console.log(stderr);
+                    callback(failOnError ? error : null);
+                });
+        }.bind(this, parameters[0], parameters[1]));
+    });
+
+    require('async').series(execs);
+})
+
+gulp.task('service:web', function() {
+    var
+        service = new (require('forever-monitor').Monitor)(
+            ['bin/console', 'server:run'], {
+                max : 3,
+                silent : false
+        });
+
+    service.on('exit', function() {
+        console.log('Web service has exited after 3 restarts');
+    });
+
+    service.start();
+});
+
+gulp.task('service:websocket', function() {
+    var
+        service = new (require('forever-monitor').Monitor)(
+            ['bin/console', 'birgit:websocket', '-vv'], {
+                max : 3,
+                silent : false
+        });
+
+    service.on('exit', function () {
+        console.log('Websocket service has exited after 3 restarts');
+    });
+
+    service.start();
+});
+
+gulp.task('service:worker', function() {
+    var
+        service = new (require('forever-monitor').Monitor)(
+            ['bin/console', 'birgit:worker', '-vv'], {
+                max : 3,
+                silent : false
+        });
+
+    service.on('exit', function () {
+        console.log('Worker service has exited after 3 restarts');
+    });
+
+    service.start();
+});
+
+gulp.task('service', ['service:web', 'service:websocket', 'service:worker']);
+
 gulp.task('default', ['css', 'webpack'], function() {
 });
--- reset.sh
+++
@@ -1,6 +0,0 @@
-bin/console doctrine:database:drop --force && \
-bin/console doctrine:database:create && \
-bin/console doctrine:schema:create --em=birgit && \
-bin/console doctrine:schema:create --em=birgit_task && \
-bin/console doctrine:schema:create --em=birgit_event && \
-bin/console birgit:test:fixtures
