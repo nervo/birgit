d66b0d9cb12eb2b5f30e05c68fd4604a9d1291bf
Florian Rey <florian.rey@elao.com>
2014-07-21 17:10:41 +0200

    Remove bower task

--- app/Resources/gulp/_tasks/bower.js
+++
@@ -1,9 +0,0 @@
-var
-    gulp        = require('gulp'),
-    gulpInstall = require('gulp-install');
-
-// Global Install - Bower
-gulp.task('install:bower', function() {
-	return gulp.src(['./bower.json'])
-  		.pipe(gulpInstall());
-});
